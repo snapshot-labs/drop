86dd46ab71589cdb40de269b4364d1fc36c1c0f6
sasicodes <me@sasi.codes>
2022-01-01 21:34:35 +0530

    Renamed to recipient

--- contracts/IDrop.sol
+++ contracts/IDrop.sol
@@ -7,7 +7,7 @@ interface IDrop {
     // Returns the merkle root of the merkle tree containing account balances available to claim.
     function merkleRoot() external view returns (bytes32);
     // Claim the given amount of the token to the given address. Reverts if the inputs are invalid.
-    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external;
+    function claim(address recipient, uint256 amount, bytes32[] calldata merkleProof) external;
     // This event is triggered whenever a call to #claim succeeds.
-    event Claimed(address account, uint256 amount);
+    event Claimed(address recipient, uint256 amount);
 }
